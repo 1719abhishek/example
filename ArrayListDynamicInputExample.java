6155e4083cac406ff5a1e4d58e6d6d7817c0f0fa
1719abhishek <112198765+1719abhishek@users.noreply.github.com>
2023-06-20 09:30:17 +0530

    Add files via upload

---
+++ ArrayListDynamicInputExample.java
@@ -0,0 +1,49 @@
+
+public class ArrayListDynamicInputExample {
+	private String holdername;
+	private String Accountnumber;
+	private String Branch;
+	private String address;
+	private int phonenumber;
+	private int deposit;
+
+	public String getHoldername() {
+		return holdername;
+	}
+	public void setHoldername(String holdername) {
+		this.holdername = holdername;
+	}
+	public String getAccountnumber() {
+		return Accountnumber;
+	}
+	public void setAccountnumber(String accountnumber) {
+		Accountnumber = accountnumber;
+	}
+	public String getBranch() {
+		return Branch;
+	}
+	public void setBranch(String branch) {
+		Branch = branch;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(int phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public int getDeposit() {
+		return deposit;
+	}
+	public void setDeposit(int deposit) {
+		this.deposit = deposit;
+	}
+	
+
+
+	}
